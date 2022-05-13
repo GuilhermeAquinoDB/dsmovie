793567b20658e7741c53a9903c5eeb509d42e897
Guilherme de Aquino <49727289+GuilhermeAquinoDB@users.noreply.github.com>
2022-05-13 01:10:56 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String email) {
+        Id = id;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
